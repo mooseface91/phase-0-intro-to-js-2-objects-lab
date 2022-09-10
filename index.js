35607e2a5c1433f6ce47722ab41e752716e0a87c
joseenriquez <co.hiker.dude@gmail.com>
2022-09-10 12:10:47 -0600

    commit tree 67f768b138f07eb11cf4a6e9546d225e674e7a3c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+let employee = {
+    name: "Jose",
+    streetAddress: "Ramona Avenue",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const jnjemployee = {...employee};
+    jnjemployee.name = "Sam";
+    jnjemployee.streetAddress = "11 Broadway";
+    return jnjemployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    let name = ["Jose"]; 
+    name.splice(1) === "Sam";
+    let streetAddress = ["Ramona Avenue"];
+    streetAddress.splice(1) === "12 Broadway";
+    return employee;
+}
